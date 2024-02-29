a1b34b6f66b338a1db7a5be7eed65a53ec50a744
sahik1234 <sahikahtesam1994@gmail.com>
2024-02-29 23:31:21 +0600

    merged with set_environment branch and run

--- lib/endpoint.dart
+++ lib/endpoint.dart
@@ -2,3 +2,8 @@ class EndPoints{
   static const String baseUrl=String.fromEnvironment('baseUrl'); // 'baseUrl' is the key name we have declared in the env directory
   static const String getInfo= "/api/getInfo";
 }
+
+//command line while calling the environment type
+/** flutter run --dart-define-file=env/dev.json **/
+/** flutter run --dart-define-file=env/prod.json **/
+/** flutter run --dart-define-file=env/stg.json **/
